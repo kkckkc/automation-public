a7da134734f52723b0bc5ee7f61f4d2c2778a57b
Magnus Johansson <kkckkc@gmail.com>
2017-12-18 18:15:44 +0100

    Fixed defect where XmlCalendarStore would generate on entry for each schedule - leading to explosion in number of events

--- events/xml.py
+++ events/xml.py
@@ -23,13 +23,17 @@ class XmlCalendarStore(CalendarStore):
         if end is None:
             end = datetime.now(tzlocal()) + dt
 
+        event_ids = set()
         index = ET.parse(self.folder + "/index.xml").getroot()
         for entry in index:
             e_start = parse_date(entry.attrib["from"])
             e_end = parse_date(entry.attrib["to"])
             if (start < e_start < end) or (start < e_end < end):
                 root = ET.parse(self.folder + "/" + entry.attrib["id"] + ".xml").getroot()
+                if entry.attrib["id"] in event_ids:
+                    continue
 
+                event_ids.add(entry.attrib["id"])
                 yield Event(
                     root.find("id").text,
                     root.find("subject").text,

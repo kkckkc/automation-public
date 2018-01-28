2694fd8bd3fc6b6fb3c43852567dc9eb764098ed
Magnus Johansson <kkckkc@gmail.com>
2018-01-28 19:06:20 +0100

    Cleanup of ios calendar

--- events/ios.py
+++ events/ios.py
@@ -6,9 +6,8 @@ from events.api import CalendarStore, Calendar, Event, Interval
 from datetime import datetime, timedelta
 from dateutil.tz import tzlocal
 from dateutil.parser import parse
-
-
 from objc_util import *
+
 import sys
 import time
 import calendar as pysyscal
@@ -20,7 +19,6 @@ EKEvent = ObjCClass('EKEvent')
 NSDate = ObjCClass('NSDate')
 
 AUTHORIZATION_STATUS_NOT_DETERMINED = 0
-AUTHORIZATION_STATUS_RESTRICTED = 1
 AUTHORIZATION_STATUS_DENIED = 2
 AUTHORIZATION_STATUS_AUTHORIZED = 3
 
@@ -28,56 +26,44 @@ class IosCalendarStore(CalendarStore):
 	def __init__(self):
 		auth_status = EKEventStore.authorizationStatusForEntityType_(0)
 		if auth_status == AUTHORIZATION_STATUS_AUTHORIZED:
-			self.event_store = EKEventStore.alloc().init()
+			self.objc_object = EKEventStore.alloc().init()
 		elif auth_status == AUTHORIZATION_STATUS_NOT_DETERMINED:
-			self.request_access()
-			self.event_store = EKEventStore.alloc().init()
+			self.__request_access()
+			self.objc_object = EKEventStore.alloc().init()
 		elif auth_status == AUTHORIZATION_STATUS_DENIED:
-			console.hud_alert('Access to calendar denied. open settings to allow')
+			console.hud_alert('Access to calendar denied.')
 
-	def request_access(self):
+	def __request_access(self):
 		def completion_handler(_cmd, success, error_ptr):
 			error = ObjCInstance(error_ptr)
-			if debug:
-				print(success)
-				print(error)
+			print(success)
+			print(error)
 			
-		completion_handler_block = ObjCBlock(completion_handler, restype=None, argtypes=[c_void_p, c_bool, c_void_p])
+		completion_handler_block = ObjCBlock(
+			completion_handler, restype=None, 
+			argtypes=[c_void_p, c_bool, c_void_p])
 	
-		EKEventStore.new().requestAccessToEntityType_completion_(0, completion_handler_block)
+		EKEventStore.new().requestAccessToEntityType_completion_(
+			0, completion_handler_block)
 	
 	def all_calendars(self):
-		objc_calendars = self.event_store.calendarsForEntityType_(0)
-		calendars = []
-		for cal in objc_calendars:
-			calendar = IosCalendar(str(cal.title()))
-			calendar.identifier = str(cal.calendarIdentifier())
-			calendar.objc_object = cal
-			calendars.append(calendar)
-		return calendars
+		objc_calendars = self.objc_object.calendarsForEntityType_(0)
+		return [IosCalendar(cal) for cal in objc_calendars]
 
-	def nsdate_from_python_date(self, date):
-		date_as_tuple = date.timetuple()
-		timestamp = pysyscal.timegm(date_as_tuple)
-		return NSDate.alloc().initWithTimeIntervalSince1970_(timestamp)
-	
 	def events(self, calendar, start=None, end=None):
-		dt = timedelta(weeks=1)
+		dt = timedelta(weeks=100)
 		if start is None:
 			start = datetime.now(tzlocal()) - dt
 		if end is None:
 			end = datetime.now(tzlocal()) + dt
 	
-		predicate = self.event_store.predicateForEventsWithStartDate_endDate_calendars_(self.nsdate_from_python_date(start), self.nsdate_from_python_date(end), [calendar.objc_object])
-		events_matching_predicate = self.event_store.eventsMatchingPredicate_(predicate)
+		predicate = self.objc_object.predicateForEventsWithStartDate_endDate_calendars_(
+			to_nsdate(start), to_nsdate(end), [calendar.objc_object])
+		events_matching_predicate = self.objc_object.eventsMatchingPredicate_(predicate)
 		if events_matching_predicate is None:
 			return []
 		else:
-			objc_events = list(events_matching_predicate)
-		
-		events = map(lambda e: IosEvent(e), objc_events)
-		return events
-	
+			return map(lambda e: IosEvent(e), events_matching_predicate)
 
 class IosEvent(Event):
 	def __init__(self, evt):
@@ -87,31 +73,32 @@ class IosEvent(Event):
 				evt.notes(), 
 				None if not evt.organizer() else evt.organizer().emailAddress(),
 				evt.location(), 
-				[Interval(
-					self.python_date_from_nsdate(evt.startDate()),
-					self.python_date_from_nsdate(evt.endDate()))
-				], 
-				[p.name().__str__() for p in evt.attendees()] if evt.attendees() else None,
+				[Interval(from_nsdate(evt.startDate()), from_nsdate(evt.endDate()))], 
+				[str(p.name()) for p in evt.attendees()] if evt.attendees() else None,
 				None, 
 				None)
-
-	def python_date_from_nsdate(self, nsdate):
-		return parse(nsdate.__str__())
+		self.objc_object = evt
 	
 class IosCalendar(Calendar):
-	def __init__(self, name):
-		super().__init__(name)		
-		
-	def __repr__(self):
-		return "IosCalendar<{}, {}>".format(self.name, self.identifier)
+	def __init__(self, cal):
+		super().__init__(str(cal.title()))		
+		self.identifier = str(cal.calendarIdentifier())
+		self.objc_object = cal	
 	
+def from_nsdate(nsdate):
+	return parse(str(nsdate))
+			
+def to_nsdate(date):
+	date_as_tuple = date.timetuple()
+	timestamp = pysyscal.timegm(date_as_tuple)
+	return NSDate.alloc().initWithTimeIntervalSince1970_(timestamp)
 	
+			
 if __name__ == '__main__':
 	store = IosCalendarStore()
 	for cal in store.all_calendars():
-#		if cal.name == 'Magnus Johansson - My Calendar':
 		print("***********************")
-		print(cal)
+		print(cal.name)
 		for evt in store.events(cal, parse("2018-01-29 08:00"), parse("2018-01-29 19:00")):
 			print(evt.id)
 			print(evt.subject)
